b817d45f1de53d2e9da00c8cbc4316bae2767343
Vinay Kumar <vinay@Mac.dlink>
2026-02-02 16:27:00 +0530

    Remove 2 test scenarios

--- tests/product-pages/cash-app.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.oappsnet.com/');
-  await page.getByRole('link', { name: 'Products' }).click();
-  await page.getByRole('link', { name: 'Products' }).click();
-});
