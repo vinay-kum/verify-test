6eb2869b98d53ba07efffed17cc6b2e5d6a9765c
Vinay Kumar <mailbox4vinay@gmail.com>
2026-02-02 16:40:13 +0530

    Delete tests/cash-app directory

--- tests/cash-app/sample-page.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.oappsnet.com/');
-  await page.getByRole('link', { name: 'Services' }).click();
-  await page.getByRole('link', { name: 'Oracle Cloud Financials' }).click();
-});
