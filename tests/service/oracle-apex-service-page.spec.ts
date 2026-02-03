e959c728ab98bd0e80e2ed181faa933640c16f2b
Vinay Kumar <vinay@Mac.dlink>
2026-02-03 22:19:59 +0530

    clean up

--- tests/service/oracle-apex-service-page.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.oracle.com/');
-  await page.getByRole('button', { name: 'Industries' }).click();
-  await page.getByRole('link', { name: 'All industries' }).click();
-  await page.getByRole('button', { name: 'Resources', exact: true }).click();
-});
--- tests/smoke/product-page.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.oappsnet.com/');
-  await page.getByRole('link', { name: 'Products' }).click();
-});
