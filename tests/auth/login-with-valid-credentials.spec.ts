93a7b74b26f467ea30cc605b620b54d634ddc479
Vinay Kumar <vinay@Vinays-MacBook-Pro.local>
2026-02-04 09:50:03 +0530

    Update "Login With Valid Credentials" test scenario

--- tests/auth/login-with-valid-credentials.spec.ts
+++ tests/auth/login-with-valid-credentials.spec.ts
@@ -1,6 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+/**
+ * Login with valid credentials
+ * Module: Auth
+ *
+ * This test was recorded with Playwright and structured by FlowProof.
+ */
+test.describe('Auth @auth', () => {
+  test('Login with valid credentials @smoke', async ({ page }) => {
   await page.goto('https://www.oappsnet.com/');
   await page.getByRole('link', { name: 'Company' }).click();
+});
 });
